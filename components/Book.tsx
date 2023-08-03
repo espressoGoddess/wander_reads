7a0a2c588a18e3fdaaddd55c85bd0609eafafcc8
Amber Shipley <ashipley2@me.com>
2023-08-03 16:51:02 -0700

    Add imports for single book data

--- components/Book.tsx
+++ components/Book.tsx
@@ -1,5 +1,11 @@
 // import data
-
+import {
+  mrFoxBookByISBN,
+  mrFoxAuthor,
+  mrFoxBookByWorks,
+} from "../data/mrFox.js";
+import Image from "next/image";
+import mrFoxCover from "../data/mrFox.jpeg";
 export default function Book() {
   // render bizz
   return <p>bizz</p>;
