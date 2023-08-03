f449dfd8105e9079c4ce7e423c144d7ebcef7977
Amber Shipley <ashipley2@me.com>
2023-08-03 16:50:06 -0700

    Add mock data for the first book

---
+++ data/mrFox.js
@@ -0,0 +1,102 @@
+export const mrFoxBookByISBN = {
+  identifiers: {
+    goodreads: ["1507552"],
+    librarything: ["6446"],
+  },
+  title: "Fantastic Mr. Fox",
+  authors: [
+    {
+      key: "/authors/OL34184A",
+    },
+  ],
+  publish_date: "October 1, 1988",
+  publishers: ["Puffin"],
+  covers: [8739161],
+  contributions: ["Tony Ross (Illustrator)"],
+  languages: [
+    {
+      key: "/languages/eng",
+    },
+  ],
+  source_records: [
+    "promise:bwb_daily_pallets_2021-05-13:KP-140-654",
+    "ia:fantasticmrfox00dahl_834",
+    "marc:marc_openlibraries_sanfranciscopubliclibrary/sfpl_chq_2018_12_24_run02.mrc:85081404:4525",
+    "amazon:0140328726",
+    "bwb:9780140328721",
+    "promise:bwb_daily_pallets_2021-04-19:KP-128-107",
+    "promise:bwb_daily_pallets_2020-04-30:O6-BTK-941",
+  ],
+  local_id: [
+    "urn:bwbsku:KP-140-654",
+    "urn:sfpl:31223064402481",
+    "urn:sfpl:31223117624784",
+    "urn:sfpl:31223113969183",
+    "urn:sfpl:31223117624800",
+    "urn:sfpl:31223113969225",
+    "urn:sfpl:31223106484539",
+    "urn:sfpl:31223117624792",
+    "urn:sfpl:31223117624818",
+    "urn:sfpl:31223117624768",
+    "urn:sfpl:31223117624743",
+    "urn:sfpl:31223113969209",
+    "urn:sfpl:31223117624750",
+    "urn:sfpl:31223117624727",
+    "urn:sfpl:31223117624776",
+    "urn:sfpl:31223117624719",
+    "urn:sfpl:31223117624735",
+    "urn:sfpl:31223113969241",
+    "urn:bwbsku:KP-128-107",
+    "urn:bwbsku:O6-BTK-941",
+  ],
+  type: {
+    key: "/type/edition",
+  },
+  first_sentence:
+    "And these two very old people are the father and mother of Mrs. Bucket.",
+  key: "/books/OL7353617M",
+  number_of_pages: 96,
+  works: [
+    {
+      key: "/works/OL45804W",
+    },
+  ],
+  ocaid: "fantasticmrfoxpu00roal",
+  isbn_10: ["0140328726"],
+  isbn_13: ["9780140328721"],
+  classifications: {},
+  latest_revision: 25,
+  revision: 25,
+  created: {
+    type: "/type/datetime",
+    value: "2008-04-29T13:35:46.876380",
+  },
+  last_modified: {
+    type: "/type/datetime",
+    value: "2023-06-12T10:24:53.657224",
+  },
+};
+
+export const mrFoxAuthor = {
+  name: "Roald Dahl",
+};
+
+export const mrFoxBookByWorks = {
+  title: "Fantastic Mr Fox",
+  key: "/works/OL45804W",
+  authors: [
+    {
+      author: {
+        key: "/authors/OL34184A",
+      },
+      type: {
+        key: "/type/author_role",
+      },
+    },
+  ],
+  type: {
+    key: "/type/work",
+  },
+  description:
+    "The main character of Fantastic Mr. Fox is an extremely clever anthropomorphized fox named Mr. Fox. He lives with his wife and four little foxes. In order to feed his family, he steals food from the cruel, brutish farmers named Boggis, Bunce, and Bean every night.\r\n\r\nFinally tired of being constantly outwitted by Mr. Fox, the farmers attempt to capture and kill him. The foxes escape in time by burrowing deep into the ground. The farmers decide to wait outside the hole for the foxes to emerge. Unable to leave the hole and steal food, Mr. Fox and his family begin to starve. Mr. Fox devises a plan to steal food from the farmers by tunneling into the ground and borrowing into the farmer's houses.\r\n\r\nAided by a friendly Badger, the animals bring the stolen food back and Mrs. Fox prepares a great celebratory banquet attended by the other starving animals and their families. Mr. Fox invites all the animals to live with him underground and says that he will provide food for them daily thanks to his underground passages. All the animals live happily and safely, while the farmers remain waiting outside in vain for Mr. Fox to show up.",
+};
