7056c6f522e56317e99585df880de1fd6e80bde7
Amber Shipley <ashipley2@me.com>
2023-08-03 16:51:13 -0700

    Create basic Book component

--- components/Book.tsx
+++ components/Book.tsx
@@ -7,6 +7,22 @@ import {
 import Image from "next/image";
 import mrFoxCover from "../data/mrFox.jpeg";
 export default function Book() {
-  // render bizz
-  return <p>bizz</p>;
+  return (
+    <article className="m-5 flex flex-col">
+      <div className="flex">
+        <Image
+          className="object-cover"
+          alt={`${mrFoxBookByISBN.title} cover`}
+          src={mrFoxCover}
+          width={100}
+          height={150}
+        />
+        <div className="flex mx-4 w-full flex-col">
+          <p className="mt-4 text-2xl">{mrFoxBookByISBN.title}</p>
+          <p className="mx-2 mt-6">By {mrFoxAuthor.name}</p>
+        </div>
+      </div>
+      <p className="mt-4 h-24 line-clamp-4">{mrFoxBookByWorks.description}</p>
+    </article>
+  );
 }
