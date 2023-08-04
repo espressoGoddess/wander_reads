c5eeab4bcab43b86f6acf49daface5f1b3ada893
Amber Shipley <ashipley2@me.com>
2023-08-04 12:46:30 -0700

    Refactor Book component to accept props

--- components/Book.tsx
+++ components/Book.tsx
@@ -1,28 +1,21 @@
-// import data
-import {
-  mrFoxBookByISBN,
-  mrFoxAuthor,
-  mrFoxBookByWorks,
-} from "../data/mrFox.js";
 import Image from "next/image";
-import mrFoxCover from "../data/mrFox.jpeg";
-export default function Book() {
+export default function Book({ ISBNInfo, cover, author, worksInfo }) {
   return (
     <article className="m-5 flex flex-col">
       <div className="flex">
         <Image
           className="object-cover"
-          alt={`${mrFoxBookByISBN.title} cover`}
-          src={mrFoxCover}
+          alt={`${ISBNInfo.title} cover`}
+          src={cover}
           width={100}
           height={150}
         />
         <div className="flex mx-4 w-full flex-col">
-          <p className="mt-4 text-2xl">{mrFoxBookByISBN.title}</p>
-          <p className="mx-2 mt-6">By {mrFoxAuthor.name}</p>
+          <p className="mt-4 text-2xl">{ISBNInfo.title}</p>
+          <p className="mx-2 mt-6">By {author.name}</p>
         </div>
       </div>
-      <p className="mt-4 h-24 line-clamp-4">{mrFoxBookByWorks.description}</p>
+      <p className="mt-4 h-24 line-clamp-4">{worksInfo.description}</p>
     </article>
   );
 }
