d92ad016e9d09678b0ee5f9b51ecae6aab2606ba
Amber Shipley <ashipley2@me.com>
2023-08-04 12:30:00 -0700

    Add data for second book

---
+++ data/manSearch.js
@@ -0,0 +1,96 @@
+export const manSearchByISBN = {
+  publishers: ["Beacon Press"],
+  number_of_pages: 165,
+  weight: "8 ounces",
+  covers: [587796],
+  physical_format: "Paperback",
+  key: "/books/OL7944580M",
+  authors: [
+    {
+      key: "/authors/OL6200391A",
+    },
+  ],
+  subjects: [
+    "Psychology",
+    "Psychotherapy - General",
+    "Historical - Holocaust",
+    "Holocaust",
+    "Psychology & Psychiatry / General",
+    "Psychopathology - Compulsive Behavior",
+    "Austria",
+    "Biography",
+    "Holocaust, Jewish (1939-1945)",
+    "Personal narratives",
+    "Psychological aspects",
+    "Psychologists",
+    "General",
+  ],
+  isbn_13: ["9780807014271"],
+  classifications: {},
+  source_records: [
+    "amazon:0807014273:df:2496044974:290170",
+    "marc:marc_loc_updates/v35.i26.records.utf8:8569391:1042",
+    "marc:marc_loc_updates/v36.i48.records.utf8:2253266:1246",
+    "marc:marc_loc_updates/v36.i49.records.utf8:5334374:1246",
+    "marc:marc_loc_updates/v36.i30.records.utf8:5238128:1246",
+    "bwb:9780807014271",
+  ],
+  title: "Man's Search for Meaning",
+  identifiers: {
+    librarything: ["918"],
+    goodreads: ["26146"],
+  },
+  languages: [
+    {
+      key: "/languages/eng",
+    },
+  ],
+  isbn_10: ["0807014273"],
+  publish_date: "June 15, 2006",
+  oclc_numbers: ["68940601"],
+  works: [
+    {
+      key: "/works/OL1268413W",
+    },
+  ],
+  type: {
+    key: "/type/edition",
+  },
+  physical_dimensions: "8.3 x 5.4 x 0.5 inches",
+  lc_classifications: ["D810.J4F72713 2006"],
+  latest_revision: 19,
+  revision: 19,
+  created: {
+    type: "/type/datetime",
+    value: "2008-04-29T15:03:11.581851",
+  },
+  last_modified: {
+    type: "/type/datetime",
+    value: "2021-09-09T23:43:42.773938",
+  },
+};
+
+export const manSearchAuthor = {
+  name: "Viktor E. Frankl",
+};
+
+export const manSearchByWorks = {
+  title: "... Trotzdem Ja zum Leben sagen",
+  first_publish_date: "December 1998",
+  key: "/works/OL1268413W",
+  authors: [
+    {
+      author: {
+        key: "/authors/OL6200391A",
+      },
+      type: {
+        key: "/type/author_role",
+      },
+    },
+  ],
+  type: {
+    key: "/type/work",
+  },
+  description:
+    "Psychiatrist Viktor Frankl's memoir has riveted generations of readers with its descriptions of life in Nazi death camps and its lessons for spiritual survival. Based on his own experience and the stories of his patients, Frankl argues that we cannot avoid suffering but we can choose how to cope with it, find meaning in it, and move forward with renewed purpose. At the heart of his theory, known as logotherapy, is a conviction that the primary human drive is not pleasure but the pursuit of what we find meaningful. Man's Search for Meaning has become one of the most influential books in America; it continues to inspire us all to find significance in the very act of living.",
+};
