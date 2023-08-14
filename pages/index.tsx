d24696b2703ddeb8e2feb69b9708d28cd00da0eb
Amber Shipley <ashipley2@me.com>
2023-08-14 15:41:40 -0700

    Revert tailwind elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import Search from "@/components/Search";
 import Shelf from "@/components/Shelf";
-import "tw-elements-react/dist/css/tw-elements-react.min.css";
 
 export default function Home() {
   return <Shelf />;
