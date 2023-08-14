5f17b82dca81ce05e6631964d4e4349d268b39b4
Amber Shipley <ashipley2@me.com>
2023-08-14 12:11:39 -0700

    Install tw elements and add logo and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import Search from "@/components/Search";
 import Shelf from "@/components/Shelf";
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
 
 export default function Home() {
   return <Shelf />;
