1d61a1d53efbaae81f731da505a93c353340b0eb
Amber Shipley <ashipley2@me.com>
2023-08-15 13:32:35 -0700

    Render header image test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
+import Search from "@/components/Search";
 import Shelf from "@/components/Shelf";
+import Image from "next/image";
 
 export default function Home() {
-  return <Shelf />;
+  return (
+    <>
+      <header>
+        <Image
+          src={"logo.svg"}
+          alt={"wander reads logo"}
+          width={500}
+          height={100}
+        />
+      </header>
+      <Search />
+    </>
+  );
 }
