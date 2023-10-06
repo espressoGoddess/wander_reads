812b09c88bb07ec5a77735ed5ac379df3b85adc1
Amber Shipley <ashipley2@me.com>
2023-10-06 15:23:29 -0700

    Implement home button functionality

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@material-tailwind/react";
 import catBook from "../public/cat-book.png";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Error() {
+  const router = useRouter();
   return (
     <section>
       <h1 className="mt-6 text-9xl text-center">404</h1>
@@ -17,7 +19,12 @@ export default function Error() {
       </div>
       <div className="text-center">
         <h2 className="mt-2">This page appears to be sleeping...</h2>
-        <Button className="mt-6" variant="outlined" ripple={true}>
+        <Button
+          onClick={() => router.push("/")}
+          className="mt-6"
+          variant="outlined"
+          ripple={true}
+        >
           Go Home
         </Button>
       </div>
